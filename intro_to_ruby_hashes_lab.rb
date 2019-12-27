6510cbd933ccfdfeb6a1c62e25da4cf56d9a7ff7
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2019-12-27 23:57:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,11 +14,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 45,
+  {:railroads=p0ces => 4, :rent_in_dollars => {:one_piece_owned => 45,
       :two_pieces_owned => 70,
       :three_pieces_owned => 23,
       :four_pieces_owned => 32,
-      :name
+      
     
   }}}
   
