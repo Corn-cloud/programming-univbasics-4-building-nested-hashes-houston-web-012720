f85f9e6e08524f45672d204ddd76e9d098932ba7
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2019-12-28 00:37:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,20 @@ def monopoly_with_third_tier
       
     
   },
-  :names => {:read_railroads => }
+  :names => {:read_railroads =>{
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800" }
   }
   
   
